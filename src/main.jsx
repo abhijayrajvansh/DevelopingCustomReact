9f4c7280c8f48f9a8fde93b39ff3913613d77fa9
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2023-11-01 01:35:59 +0530

    Chapter-01

--- src/main.jsx
+++ src/main.jsx
@@ -8,15 +8,23 @@ import {
   createBrowserRouter,
   createRoutesFromElements,
 } from "react-router-dom";
+
 import Layout from "./Layout.jsx";
+import Home from './components/Home/Home.jsx'
+
 
 const router = createBrowserRouter(
-  createRoutesFromElements(<Route path="/" element={<Layout />}></Route>)
-);
+  createRoutesFromElements(
+    <Route path='/' element={<Layout />}>
+     <Route path='/' element={<Home />}/>
+     
+    </Route>
+  )
+)
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* <App /> */}
+
     <RouterProvider router={router} />
   </React.StrictMode>
 );
